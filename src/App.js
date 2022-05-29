e81efb18df5003ed9c3a885b1f95df71042d26da
ASUS <Yehor Kholodiuk>
2022-05-29 19:44:55 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
+const initialList = [
+  {id:123,name:'ABC',status:'todo'},
+  {id:234,name:'Learn React',status:'progress'},
+  {id:456,name:'JavaScript',status:'review'}
+]
 function App() {
+  const [taskName,setTaskName] = useState('')
+  const [list,setList] = useState(initialList)
+
+  const addTask = () => {
+    console.log(taskName)
+    setList ([...list,{id:Math.random(), name:taskName, status:'todo'}]);
+    setTaskName('')
+  }
+
+  const deleteTask = (id) => {
+    const newList = list.filter ( el => el.id !== id)
+    setList(newList)
+  }
+
+  const changeStatusDone = (id) => {
+    const n = list.map(el => el.id === id? {...el, status:'done'}:el)
+    setList(n)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>Task:{taskName}</h1>
+      <input value={taskName} onChange={e => setTaskName(e.target.value)}/>
+      <button onClick={addTask}>Add Task</button>
+        <ul>
+            {
+                list.map(el => <li key={el.id} className={el.status}>{el.name}<strong>{el.status}</strong>
+
+                <button onClick={() => deleteTask(el.id)}>Delete</button>
+                <button onClick={() => changeStatusDone(el.id)}>Done</button>
+
+                </li>)
+            }
+        </ul>
     </div>
   );
 }
