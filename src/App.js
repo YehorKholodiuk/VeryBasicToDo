a50e72e22aaecde4e9237ce7ce22174f0b669c45
ASUS <Yehor Kholodiuk>
2022-05-30 14:43:28 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+
 const initialList = [
-  {id:123,name:'ABC',status:'todo'},
-  {id:234,name:'Learn React',status:'progress'},
-  {id:456,name:'JavaScript',status:'review'}
+    {id: 123, name: 'ABC', status: 'todo'},
+    {id: 234, name: 'Learn React', status: 'progress'},
+    {id: 456, name: 'JavaScript', status: 'review'}
 ]
+
 function App() {
-  const [taskName,setTaskName] = useState('')
-  const [list,setList] = useState(initialList)
+    const [taskName, setTaskName] = useState('')
+    const [list, setList] = useState(initialList)
 
-  const addTask = () => {
-    console.log(taskName)
-    setList ([...list,{id:Math.random(), name:taskName, status:'todo'}]);
-    setTaskName('')
-  }
+    const addTask = () => {
+        console.log(taskName)
+        setList([...list, {id: Math.random(), name: taskName, status: 'todo'}]);
+        setTaskName('')
+    }
 
-  const deleteTask = (id) => {
-    const newList = list.filter ( el => el.id !== id)
-    setList(newList)
-  }
+    const deleteTask = (id) => {
+        const newList = list.filter(el => el.id !== id)
+        setList(newList)
+    }
 
-  const changeStatusDone = (id) => {
-    const n = list.map(el => el.id === id? {...el, status:'done'}:el)
-    setList(n)
-  }
-
-
-  const selectTaskStatus = (id,event) => {
-  setList(list.map(el => el.id === id ? {...el,status:event.target.value}:el))
-  }
+    const changeStatusDone = (id) => {
+        const n = list.map(el => el.id === id ? {...el, status: 'done'} : el)
+        setList(n)
+    }
 
 
-  return (
-    <div className="App">
-<h1>Task:{taskName}</h1>
-      <input value={taskName} onChange={e => setTaskName(e.target.value)}/>
-      <button onClick={addTask}>Add Task</button>
-        <ul>
-            {
-                list.map(el => <li key={el.id} className={el.status}>{el.name}<strong>{el.status}</strong>
-                <select value={el.status} onChange = {event => selectTaskStatus(el.id,event)}>
-                  <option value="todo">todo</option>
-                  <option value="progress">progress</option>
-                  <option value="review">review</option>
+    const selectTaskStatus = (id, event) => {
+        setList(list.map(el => el.id === id ? {...el, status: event.target.value} : el))
+    }
 
 
-                </select>
-                <button onClick={() => deleteTask(el.id)}>Delete</button>
-                <button onClick={() => changeStatusDone(el.id)}>Done</button>
+    return (
+        <div className="App">
+            <h1>Task:{taskName}</h1>
+            <input value={taskName} onChange={e => setTaskName(e.target.value)}/>
+            <button onClick={addTask}>Add Task</button>
+            <ul>
+                {
+                    list.map(el => <li key={el.id} className={el.status}>{' '}{el.name}<strong>{el.status}{' '}</strong>
+                        <select value={el.status} onChange={event => selectTaskStatus(el.id, event)}>
+                            <option value="todo">todo</option>
+                            <option value="progress">progress</option>
+                            <option value="review">review</option>
 
-                </li>)
-            }
-        </ul>
-    </div>
-  );
+
+                        </select>
+                        <button onClick={() => deleteTask(el.id)}>Delete</button>
+                        <button onClick={() => changeStatusDone(el.id)}>Done</button>
+
+                    </li>)
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
